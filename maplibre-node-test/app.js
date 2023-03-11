0a2a5afd633927acc80b2a4f9dd5f2754abfb904
tdcosta100 <tdcosta100@gmail.com>
2023-03-11 01:30:46 -0300

    Make the approaches sequential

--- maplibre-node-test/app.js
+++ maplibre-node-test/app.js
@@ -83,45 +83,45 @@ request('https://tiles.wifidb.net/styles/WDB_OSM/style.json', function (err, res
             if (err) throw err;
         });
     }
-});
+    
+    // Approach 2
+    request('https://tiles.wifidb.net/styles/WDB_OSM/style.json', function (err, res, body) {
+        if (err) throw err;
+        if (res.statusCode == 200) {
+            let style = JSON.parse(body);
 
-// Approach 2
-request('https://tiles.wifidb.net/styles/WDB_OSM/style.json', function (err, res, body) {
-    if (err) throw err;
-    if (res.statusCode == 200) {
-        let style = JSON.parse(body);
+            // MODIFY STYLE HERE if desired
 
-        // MODIFY STYLE HERE if desired
+            map.load(style);
 
-        map.load(style);
-        
-        const renderImage = filename => {
-            map.render((err, buffer) => {
-                if (err) throw err;
-
-                let image = sharp(buffer, {
-                    raw: {
-                        width: 512,
-                        height: 512,
-                        channels: 4
-                    }
-                });
-                
-                // Convert raw image buffer to PNG
-                image.toFile(filename, function (err) {
+            const renderImage = filename => {
+                map.render((err, buffer) => {
                     if (err) throw err;
+
+                    let image = sharp(buffer, {
+                        raw: {
+                            width: 512,
+                            height: 512,
+                            channels: 4
+                        }
+                    });
+
+                    // Convert raw image buffer to PNG
+                    image.toFile(filename, function (err) {
+                        if (err) throw err;
+                    });
                 });
-            });
+            }
+
+            map.setCenter([-98.5795, 39.8282]);
+            map.setZoom(13);
+            renderImage('Z13.png');
+
+            map.setZoom(10);
+            renderImage('Z10.png');
+
+            map.setCenter([-88.5795, 39.8282]);
+            renderImage('Z10_2.png');
         }
-        
-        map.setCenter([-98.5795, 39.8282]);
-        map.setZoom(13);
-        renderImage('Z13.png');
-
-        map.setZoom(10);
-        renderImage('Z10.png');
-
-        map.setCenter([-88.5795, 39.8282]);
-        renderImage('Z10_2.png');
-    }
+    });
 });
